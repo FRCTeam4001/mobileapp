02fc3a89b449a77d5d32b9d51c1ee9fb0c4b6f2c
Safwan Choudhury <im@safwanc.com>
2015-02-12 23:03:34 -0500

    Initial Commit

---
+++ Team4001/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Team4001
+//
+//  Created by Safwan on 2015-02-12.
+//  Copyright (c) 2015 FRC4001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
