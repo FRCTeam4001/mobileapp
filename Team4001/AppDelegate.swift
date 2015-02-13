73040cc670b387412623243d99460218f160af74
Safwan Choudhury <im@safwanc.com>
2015-02-12 23:28:43 -0500

    Adding basic app which loads website

--- Team4001/AppDelegate.swift
+++ Team4001/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        // Create a new UIWindow object and set the WebsiteViewController as the root view controller.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window!.rootViewController = WebsiteViewController()
         return true
     }
 }
