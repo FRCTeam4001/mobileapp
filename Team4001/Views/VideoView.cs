e49694e7e4f62ac1d0424f28b90e42ddcdcaac4d
Safwan Choudhury <im@safwanc.com>
2015-03-24 21:59:09 -0400

    Added support for videos
    - Added a custom VideoView for Xamarin.Forms
    - Added a custom VideoViewRenderer for Xamarin.Forms iOS
    - Included “Totes” video as a resource for iOS

---
+++ Team4001/Views/VideoView.cs
@@ -0,0 +1,30 @@
+﻿//
+// VideoView.cs
+//
+// Author:
+//       Safwan Choudhury <safwan.choudhury@quanser.com>
+//
+// Copyright (c) 2015 Quanser Inc.
+//
+using System;
+
+using Xamarin.Forms;
+
+namespace Team4001
+{
+    public class VideoView : View
+    {
+        public string Filename { get; private set; }
+
+        public float Width { get; private set; }
+
+        public float Height { get; private set; }
+
+        public VideoView(string Filename, float Width, float Height)
+        {
+            this.Filename = Filename; 
+            this.Width = Width; 
+            this.Height = Height; 
+        }
+    }
+}
---
+++ Team4001/iOS/Renderers/VideoViewRenderer.cs
@@ -0,0 +1,57 @@
+﻿//
+// VideoViewRenderer.cs
+//
+// Author:
+//       Safwan Choudhury <safwan.choudhury@quanser.com>
+//
+// Copyright (c) 2015 Quanser Inc.
+//
+using Team4001;
+using Team4001.iOS;
+
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(VideoView), typeof(VideoViewRenderer))]
+namespace Team4001.iOS
+{
+    using System;
+    using System.Drawing;
+
+    using Foundation;
+    using UIKit;
+    using MediaPlayer;
+
+    using Xamarin.Forms.Platform.iOS;
+
+    public class VideoViewRenderer : ViewRenderer
+    {
+        private const float WIDTH_PERCENTAGE = 0.9f;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<View> e)
+        {
+            base.OnElementChanged(e);
+
+            var video = (VideoView)e.NewElement;
+            var a = video.Height / video.Width;                                      // height / width
+            var w = (float)(UIScreen.MainScreen.Bounds.Width) * WIDTH_PERCENTAGE;    // desired width
+            var h = w * a;                                                           // calculated height
+
+            var player = new MPMoviePlayerController(NSUrl.CreateFileUrl(new [] { NSBundle.MainBundle.PathForResource("Totes", "mp4") }))
+            {
+                ShouldAutoplay = false,
+            };
+            
+            player.PrepareToPlay();
+
+           
+
+            var view = new UIView(new RectangleF(0, 0, w, h));
+
+            player.View.Frame = view.Bounds; 
+            view.AddSubview(player.View);
+
+            SetNativeControl(view);
+        }
+    }
+}
+
