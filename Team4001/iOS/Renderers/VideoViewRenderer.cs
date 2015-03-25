65997e2f92b73581cf4d8b5808538bde7021cf02
Safwan Choudhury <im@safwanc.com>
2015-03-25 12:07:45 -0400

    Video player now working on hardware!
    WOOOHOOOO

--- Team4001/iOS/Renderers/VideoViewRenderer.cs
+++ Team4001/iOS/Renderers/VideoViewRenderer.cs
@@ -19,13 +19,22 @@ namespace Team4001.iOS
 
     using Foundation;
     using UIKit;
-    using MediaPlayer;
+    using ObjCRuntime;
+    using AVFoundation;
+    using CoreMedia;
 
     using Xamarin.Forms.Platform.iOS;
 
     public class VideoViewRenderer : ViewRenderer
     {
         private const float WIDTH_PERCENTAGE = 0.9f;
+
+        private AVPlayer player; 
+        private AVPlayerLayer playerLayer; 
+        private AVAsset asset; 
+        private AVPlayerItem playerItem; 
+
+        private bool playing; 
 
         protected override void OnElementChanged(ElementChangedEventArgs<View> e)
         {
@@ -39,19 +48,47 @@ namespace Team4001.iOS
             var w = maxWidth * WIDTH_PERCENTAGE;    // desired width
             var h = w * a;                          // computed height
 
-            var player = new MPMoviePlayerController(NSUrl.FromFilename(video.Filename))
-            {
-                ShouldAutoplay = false,
-            };
-            
-            player.PrepareToPlay();           
-
             var view = new UIView(new RectangleF(0, 0, w, h));
 
-            player.View.Frame = view.Bounds; 
-            view.AddSubview(player.View);
+            asset = AVAsset.FromUrl(NSUrl.FromFilename(video.Filename));
+            playerItem = new AVPlayerItem(asset); 
+            player = new AVPlayer(playerItem); 
+            playerLayer = AVPlayerLayer.FromPlayer(player); 
+            playerLayer.Frame = view.Frame; 
+            view.Layer.AddSublayer(playerLayer); 
+
+            var tapGestureRecognizer = new UITapGestureRecognizer((obj) =>
+            {
+                if (player != null)
+                {
+                    if (playing)
+                    {
+                        player.Pause(); 
+                        playing = false; 
+                    }
+                    else
+                    {
+                        player.Play(); 
+                        playing = true; 
+                    }
+                }
+            }); 
+
+            NSNotificationCenter.DefaultCenter.AddObserver(this, new Selector("playerItemDidPlayToEnd:"), AVPlayerItem.DidPlayToEndTimeNotification, playerItem);
+
+            view.AddGestureRecognizer(tapGestureRecognizer); 
 
             SetNativeControl(view);
+        }
+
+        [Export("playerItemDidPlayToEnd:")]
+        public void PlaybackEnded(NSNotification n)
+        {
+            if (playerItem != null)
+            {
+                playerItem.Seek(CMTime.Zero); 
+                playing = false; 
+            }
         }
     }
 }
