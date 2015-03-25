eeb1477118b935d77bd6db72764a84469abd8673
Safwan Choudhury <im@safwanc.com>
2015-03-25 12:58:38 -0400

    Added Late O'Clock and Rogers Daytime

--- Team4001/Pages/MediaPage.cs
+++ Team4001/Pages/MediaPage.cs
@@ -30,6 +30,18 @@ namespace Team4001
                     Children =
                     {
                         new CardView(
+                            new HeadingView("Late O'Clock News"),
+                            new VideoView("LateOClockNews.mp4", 854.0f, 476.0f),
+                            new ParagraphView("Team 4001 appears on Late O'Clock News with Paul Lemieux!")
+                        ),
+
+                        new CardView(
+                            new HeadingView("Rogers Daytime Television"),
+                            new VideoView("RogersDaytime.mp4", 596.0f, 336.0f),
+                            new ParagraphView("Team 4001 appears on Rogers Daytime Television!")
+                        ),
+
+                        new CardView(
                             new HeadingView("All About Them Totes"),
                             new VideoView("Totes.mp4", 640.0f, 360.0f),
                             new ParagraphView("A video we created for the 2015 FIRST Robotics Challenge: Recycle Rush.")
